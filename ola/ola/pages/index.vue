ababe7817e705e8ad4149dd99fdd787e1eed788d
Oguzhanozturkkk <102763507+Oguzhanozturkkk@users.noreply.github.com>
2022-12-08 04:51:40 +0300

    index sayfası yapıldı

---
+++ ola/ola/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <headForm/>
+    <ListForm/>
+    <!-- <corousel/> -->
+    <deneme/>
+    <cards/>
+    <br>
+    <shop2/>
+
+   
+  </div>
+  
+ 
+</template>
+<script>
+
+
+</script>
